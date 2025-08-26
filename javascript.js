7e37394ac626cd645e5b2b2bcf4965fd2c8c0d18
Eleanor <131412056+elliedeli@users.noreply.github.com>
2025-08-26 12:43:09 -0500

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,26 +1,54 @@
 console.log("Hello world!");
 async function populate(){
-    const data = await fetch('listings.json');
-    const json = await data.json();
-    thing(json)
+    const requestURL = 'https://raw.githubusercontent.com/elliedeli/airbnb-listings/refs/heads/main/listings.json';
+    const request = new Request(requestURL);
+    const response = await fetch(request);
+    const json = await response.json();
+    populateListings(json)
 }
 
-function thing(obj){
-    const row = document.querySelector("row");
+function populateListings(obj){
+    const container = document.querySelector(".container");
     const listings = obj.listings;
     for (const listing of listings)
     {
-        
+        //create Elements based off Data   
         const listingArticle = document.createElement("article");
         const listingName = document.createElement("h2");
         const listingInfo = document.createElement("ul");
         const listingDesc = document.createElement("p");
+        const listingButton = document.createElement("button");
+
+        //add text content to elements
         listingName.textContent = listing.name;
         listingDesc.textContent = listing.desc;
         
+        //stuff with ID (url and image)
+        var listingUrl = "https://airbnb.com/h/" + listing.id;
+        console.log(listingUrl);
+        var listingImageUrl = "./media/" + listing.id + ".jpeg";
+        console.log(listingImageUrl);
+
+        //image
+        const listingImage = document.createElement("img");
+        listingImage.src = listingImageUrl;
+        listingImage.width = "300";
+        listingImage.height = "300";
+  
+        //button
+        listingButton.textContent = "Book Now!"
+        const anchorElement = document.createElement("a");
+        anchorElement.href = listingUrl;
+        anchorElement.target = "_blank"
+        anchorElement.rel = "noopener noreferrer"
+
+        //appending Elements
+        anchorElement.appendChild(listingButton)
         listingArticle.appendChild(listingName);
+        listingArticle.appendChild(listingImage);
         listingArticle.appendChild(listingDesc);
-        row.appendChild(listingArticle);
+        listingArticle.appendChild(anchorElement);
+        container.appendChild(listingArticle);
 
     }
 }
